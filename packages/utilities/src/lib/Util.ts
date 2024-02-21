8201960b0c4c5bb02e3c037709415405e9d33aa8
satoufuyuki <satoufuyuki@proton.me>
2024-02-21 21:46:20 +0700

    feat(packages/utilities): remove BaseClient from constructor

--- packages/utilities/src/lib/Util.ts
+++ packages/utilities/src/lib/Util.ts
@@ -1,10 +1,8 @@
 import fs from "node:fs/promises";
 import { URL } from "node:url";
-import type { BaseClient } from "@animeworldid/core";
 
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Util {
-    public constructor(private readonly client: BaseClient) {}
-
     public static async loadJSON<T>(path: string): Promise<T> {
         const file = await fs.readFile(new URL(path, import.meta.url));
         return JSON.parse(file as unknown as string) as T;
