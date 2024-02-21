f1f420296b98c88e2e90a32f4b40b7cae9f78d58
satoufuyuki <satoufuyuki@proton.me>
2024-02-21 21:47:12 +0700

    fix(services/general): appropriate gateway database url and base user dir

--- services/general/src/index.ts
+++ services/general/src/index.ts
@@ -1,7 +1,8 @@
 import "source-map-support/register.js";
 import path from "node:path";
+import process from "node:process";
 import { fileURLToPath } from "node:url";
-import { BaseClient, amqpUrl, databaseUrl } from "@animeworldid/core";
+import { BaseClient, amqpUrl, discordToken, gatewayDatabaseUrl } from "@animeworldid/core";
 import { Util } from "@animeworldid/utilities";
 import { createBanner } from "@skyra/start-banner";
 import gradient from "gradient-string";
@@ -9,9 +10,11 @@ import gradient from "gradient-string";
 const packageJson = await Util.loadJSON<{ version: string; name: string; }>(`file://${path.join(fileURLToPath(import.meta.url), "../../package.json")}`);
 
 const client = new BaseClient({
-    baseUserDirectory: path.resolve(path.dirname(import.meta.url), ".."),
+    baseUserDirectory: path.join(process.cwd(), "dist"),
     amqpUrl,
-    databaseUrl
+    databaseUrl: gatewayDatabaseUrl,
+    token: discordToken,
+    registerCommands: true
 }, packageJson.name);
 
 console.log(
